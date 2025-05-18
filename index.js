3fc24db32c8c7b4e5d6e8f227f923f6802f46ff8
wawaimut <wawaferdy9@gmail.com>
2025-05-18 06:07:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+burger=document.querySelector('.burger')
+navbarItems=document.querySelector('.navbar-items')
+nav=document.querySelector('.nav')
+
+burger.addEventListener('click',()=>{
+    navbarItems.classList.toggle('h-class')
+    nav.classList.toggle('v-class')
+ })
+
+ 
